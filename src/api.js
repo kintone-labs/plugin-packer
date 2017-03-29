4c1d63ef6465dfedc1ff75a78b3320b5e20f8daf
teppeis <teppeis@gmail.com>
2017-03-29 13:40:12 +0900

    refactor: separate api from packer

---
+++ src/api.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const archiver = require('archiver');
+const RSA = require('node-rsa');
+const streamBuffers = require('stream-buffers');
+const debug = require('debug')('api');
+
+const sign = require('./sign');
+const uuid = require('./calc-uuid');
+
+/**
+ * @param {Buffer} contentsZip The zipped plugin contents directory.
+ * @param {string=} privateKey The private key (PKCS#1 PEM).
+ * @return {Promise<{plugin: Buffer, privateKey: string, id: string}>}
+ */
+function packer(contentsZip, privateKey) {
+  return Promise.resolve().then(() => {
+    let key;
+    if (privateKey) {
+      key = new RSA(privateKey);
+    } else {
+      debug('generating a new key');
+      key = new RSA({b: 1024});
+      privateKey = key.exportKey('pkcs1-private');
+    }
+
+    const signature = sign(contentsZip, privateKey);
+    const publicKey = key.exportKey('pkcs8-public-der');
+
+    const id = uuid(publicKey);
+    debug(`id : ${id}`);
+    return zip(contentsZip, publicKey, signature)
+      .then(plugin => ({plugin: plugin, privateKey: privateKey, id: id}));
+  });
+}
+
+/**
+ * Create plugin.zip
+ *
+ * @param {!Buffer|!stream.Readable|string} contentsZip
+ * @param {!Buffer|!stream.Readable|string} publicKey
+ * @param {!Buffer|!stream.Readable|string} signature
+ * @return {!Promise<Buffer>}
+ */
+function zip(contentsZip, publicKey, signature) {
+  return new Promise((res, rej) => {
+    const output = new streamBuffers.WritableStreamBuffer();
+    const archive = archiver('zip');
+    output.on('finish', () => {
+      debug(`plugin.zip: ${archive.pointer()} bytes`);
+      res(output.getContents());
+    });
+    archive.pipe(output);
+    archive.on('error', e => {
+      rej(e);
+    });
+    archive.append(contentsZip, {name: 'contents.zip'});
+    archive.append(publicKey, {name: 'PUBKEY'});
+    archive.append(signature, {name: 'SIGNATURE'});
+    archive.finalize();
+  });
+}
+
+module.exports = packer;
--- src/index.js
+++ src/index.js
@@ -3,21 +3,20 @@
 const path = require('path');
 const fs = require('fs');
 const archiver = require('archiver');
-const RSA = require('node-rsa');
 const denodeify = require('denodeify');
+const writeFile = denodeify(fs.writeFile);
 const recursive = denodeify(require('recursive-readdir'));
 const streamBuffers = require('stream-buffers');
 const debug = require('debug')('packer');
 
-const sign = require('./sign');
-const uuid = require('./calc-uuid');
+const packer = require('./api');
 
 /**
  * @param {string} pluginDir path to plugin directory.
  * @param {Object=} options {ppk: string}.
  * @return {Promise<string>} The resolved value is a path to the output plugin zip file.
  */
-function packer(pluginDir, options) {
+function cli(pluginDir, options) {
   options = options || {};
 
   // 1. check if pluginDir is a directory
@@ -31,58 +30,42 @@ function packer(pluginDir, options) {
   }
 
   const outputDir = path.dirname(path.resolve(pluginDir));
-  debug(`outDir: ${outputDir}`);
+  debug(`outDir : ${outputDir}`);
 
   return recursive(pluginDir).then(files => {
     files.forEach(file => {
       const basename = path.basename(file);
       // 3. check dot files
       if (/^\./.test(basename)) {
-        throw new Error(`PLUGIN_DIR must not contain dot files or directories: ${file}`);
+        throw new Error(`PLUGIN_DIR must not contain dot files or directories : ${file}`);
       }
       // 4. check *.ppk
       if (/\.ppk$/.test(basename)) {
-        throw new Error(`PLUGIN_DIR must not contain *.ppk: ${file}`);
+        throw new Error(`PLUGIN_DIR must not contain * .ppk : ${file}`);
       }
     });
   }).then(() => {
     // 5. generate new ppk if not specified
     const ppkFile = options.ppk;
-    let key;
     let privateKey;
     if (ppkFile) {
       debug(`loading an existing key: ${ppkFile}`);
       privateKey = fs.readFileSync(ppkFile, 'utf8');
-      key = new RSA(privateKey);
-    } else {
-      debug('generating a new key');
-      key = new RSA({b: 1024});
-      privateKey = key.exportKey('pkcs1-private');
     }
 
-    // 6. zip plugin contents
-    return createContentsZip(pluginDir).then(contentsZip => {
-      // 7. sign to contents
-      const signature = sign(contentsZip, privateKey);
-
-      // 8. export public key
-      const publicKey = key.exportKey('pkcs8-public-der');
-
-      if (!ppkFile) {
-        // 9. calc UUID
-        const id = uuid(publicKey);
-        debug(`id: ${id}`);
-        // 10. output private key if nothing
-        fs.writeFileSync(path.join(outputDir, `${id}.ppk`), privateKey, 'utf8');
-      }
-      // 11. zip plugin
-      const outputPath = path.join(outputDir, 'plugin.zip');
-      return outputPlugin(outputPath, contentsZip, publicKey, signature);
-    });
+    // 6. package plugin.zip
+    return createContentsZip(pluginDir)
+      .then(contentsZip => packer(contentsZip, privateKey))
+      .then(output => {
+        if (!ppkFile) {
+          fs.writeFileSync(path.join(outputDir, `${output.id}.ppk`), output.privateKey, 'utf8');
+        }
+        return outputPlugin(outputDir, output.plugin);
+      });
   });
 }
 
-module.exports = packer;
+module.exports = cli;
 
 /**
  * Create contents.zip
@@ -110,27 +93,12 @@ function createContentsZip(pluginDir) {
 /**
  * Create and save plugin.zip
  *
- * @param {string} outputPath
- * @param {!Buffer|!stream.Readable|string} contentsZip
- * @param {!Buffer|!stream.Readable|string} publicKey
- * @param {!Buffer|!stream.Readable|string} signature
- * @return {!Promise<undefined>}
+ * @param {string} outputDir
+ * @param {!Buffer} plugin
+ * @return {!Promise<string>} The value is output path of plugin.zip.
  */
-function outputPlugin(outputPath, contentsZip, publicKey, signature) {
-  return new Promise((res, rej) => {
-    const output = fs.createWriteStream(outputPath);
-    const archive = archiver('zip');
-    output.on('close', () => {
-      debug(`plugin.zip: ${archive.pointer()} bytes`);
-      res(outputPath);
-    });
-    archive.pipe(output);
-    archive.on('error', e => {
-      rej(e);
-    });
-    archive.append(contentsZip, {name: 'contents.zip'});
-    archive.append(publicKey, {name: 'PUBKEY'});
-    archive.append(signature, {name: 'SIGNATURE'});
-    archive.finalize();
-  });
+function outputPlugin(outputDir, plugin) {
+  const outputPath = path.join(outputDir, 'plugin.zip');
+  return writeFile(outputPath, plugin)
+    .then(arg => outputPath);
 }
