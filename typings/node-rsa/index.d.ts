e346d86a3182791f38e5df9fd9a47d83e59e05c7
teppeis <teppeis@gmail.com>
2017-05-02 18:27:53 +0900

    fix: add typings for node-rsa

--- typings/node-rsa/index.d.ts
+++ typings/node-rsa/index.d.ts
@@ -1,2 +1,7 @@
 declare module 'node-rsa' {
+    class RSA {
+        constructor(keyData: string|Buffer|Object, format: string, options?: Object);
+        sign(buffer: Buffer, encoding?: string, source_encoding?: string): Buffer;
+    }
+    export = RSA;
 }
