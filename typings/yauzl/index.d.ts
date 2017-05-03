dbd2fffe4eae86b0290d70ace9d65e7ffd3e2a58
teppeis <teppeis@gmail.com>
2017-05-03 23:46:09 +0900

    fix: overloads order in yauzl typings

--- typings/yauzl/index.d.ts
+++ typings/yauzl/index.d.ts
@@ -3,16 +3,16 @@ declare module 'yauzl' {
     import {Readable} from 'stream';
     import {EventEmitter} from 'events';
 
-    export function fromBuffer(buffer: Buffer, callback: (err: Error, zipFile: ZipFile) => void): void;
     export function fromBuffer(buffer: Buffer, options: any, callback: (err: Error, zipFile: ZipFile) => void): void;
+    export function fromBuffer(buffer: Buffer, callback: (err: Error, zipFile: ZipFile) => void): void;
     export class ZipFile extends EventEmitter {
         on(event: 'entry', callback: (entry: Entry) => void): this;
         on(event: 'end', callback: () => void): this;
         on(event: 'close', callback: () => void): this;
         on(event: 'error', callback: (err: Error) => void): this;
         readEntry(): void;
-        openReadStream(entry: Entry, callback: (err: Error, stream: Readable) => void): void;
         openReadStream(entry: Entry, options: any, callback: (err: Error, stream: Readable) => void): void;
+        openReadStream(entry: Entry, callback: (err: Error, stream: Readable) => void): void;
     }
     export class Entry {
         versionMadeBy: number;
