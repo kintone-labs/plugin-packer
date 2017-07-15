9172b944ab7142a4bdd0033877061d4f67879f05
teppeis <teppeis@gmail.com>
2017-07-15 15:04:54 +0900

    chore: use @types/mkdirp@0.5.0

--- typings/mkdirp/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'mkdirp' {
-    function mkdirp(dir: string, cb: (err: Error, made: string) => void): void;
-    function mkdirp(dir: string, opts: any, cb: (err: Error, made: string) => void): void;
-    namespace mkdirp {
-        function sync(dir: string, opts?: any): string;
-    }
-    export = mkdirp;
-}
